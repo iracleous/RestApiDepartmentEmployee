103d196ef19c7f5312c8fb8a8b73f47650b1f2e2
dimitris <iracleous@gmail.com>
2021-12-06 21:14:45 +0200

    More operations

--- src/main/java/gr/codehub/MainApplication.java
+++ src/main/java/gr/codehub/MainApplication.java
@@ -11,7 +11,7 @@ public class MainApplication {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
         String urlForDepartment= "http://localhost:4000/department";
-        String urlForEmployee= "http://localhost:4000/employee";
+
 
         Department department = new Department();
         department.setTitle("Administration");
@@ -19,18 +19,50 @@ public class MainApplication {
 
         restTemplate.postForEntity(urlForDepartment,department,Department.class);
 
-        ResponseEntity<List> response
-                = restTemplate.getForEntity(urlForDepartment , List.class);
-
-        System.out.println(response.getBody().toString());
 
 
-        ResponseEntity<List> employeeList
-                = restTemplate.getForEntity(urlForEmployee , List.class);
+        ResponseEntity<Department> employeeList
+                = restTemplate.getForEntity(urlForDepartment+"/1", Department.class);
 
         System.out.println(employeeList.getBody().toString());
 
     }
 
+    public static void test2() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String urlGoogle= "https://gorest.co.in/public/v1/users";
+
+        ResponseEntity<Object> response
+                = restTemplate.getForEntity(urlGoogle, Object.class);
+
+        System.out.println(response.getBody());
+
+
+    }
+
+
+public static void test1(){
+    RestTemplate restTemplate = new RestTemplate();
+    String urlForDepartment= "http://localhost:4000/department";
+    String urlForEmployee= "http://localhost:4000/employee";
+
+    Department department = new Department();
+    department.setTitle("Administration");
+    department.setLocation("Patra");
+
+    restTemplate.postForEntity(urlForDepartment,department,Department.class);
+
+    ResponseEntity<List> response
+            = restTemplate.getForEntity(urlForDepartment, List.class);
+
+    System.out.println(response.getBody().toString());
+
+
+    ResponseEntity<List> employeeList
+            = restTemplate.getForEntity(urlForEmployee, List.class);
+
+    System.out.println(employeeList.getBody().toString());
+}
 
 }
---
+++ src/main/java/gr/codehub/model/User.java
@@ -0,0 +1,16 @@
+package gr.codehub.model;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+    private String email;
+   private String gender;
+    private String status;
+
+
+
+}
